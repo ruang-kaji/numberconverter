8934080c0087f9a51573b8ab4917cb3a1bd40668
Mr_NeeT <abdoelrachmad@gmail.com>
2020-06-27 03:50:11 +0700

    Adding first layout's.

---
+++ app/src/main/java/com/ruangkaji/numberconverter/main/MainActivity.java
@@ -0,0 +1,47 @@
+package com.ruangkaji.numberconverter.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.ruangkaji.numberconverter.R;
+import com.ruangkaji.numberconverter.main.fragments.BinaryFragment;
+import com.ruangkaji.numberconverter.main.fragments.HexadecimalFragment;
+import com.ruangkaji.numberconverter.main.fragments.OctalFragment;
+import com.ruangkaji.numberconverter.utils.TabAdapter;
+import com.ruangkaji.numberconverter.utils.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabAdapter tabAdapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setTheme(R.style.AppTheme);
+        setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.fragment_container);
+        tabLayout = findViewById(R.id.top_nav);
+
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        tabAdapter.addFragment(
+                getString(R.string.main_fragment_decimal_title),
+                new HexadecimalFragment());
+        tabAdapter.addFragment(
+                getString(R.string.main_fragment_binary_title),
+                new BinaryFragment());
+        tabAdapter.addFragment(
+                getString(R.string.main_fragment_octal_title),
+                new OctalFragment());
+        tabAdapter.addFragment(
+                getString(R.string.main_fragment_hexadecimal_title),
+                new HexadecimalFragment());
+
+        viewPager.setAdapter(tabAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/com/ruangkaji/numberconverter/main/fragments/DecimalFragment.java
@@ -0,0 +1,32 @@
+package com.ruangkaji.numberconverter.main.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.ruangkaji.numberconverter.R;
+
+public class DecimalFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState
+    ) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        return inflater.inflate(R.layout.fragment_main_decimal, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
